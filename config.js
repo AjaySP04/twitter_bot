a8b8e5c29c3de2d5415c8b1b40bd32117fdb2e24
Ajay Singh Parmar <ajays.parmar04@gmail.com>
2019-01-06 02:28:39 +0530

    Add files via upload
    This is a second commit for the Tweets favorite twitter bot.

---
+++ config.js
@@ -0,0 +1,9 @@
+//config.js
+
+module.exports = {
+	consumer_key: 'e5R9z0EKgEK6MReATDoPYgICR',
+	consumer_secret: 'PywE66JtyE7PaLqX9Uv6L6kmPnme22N2VELGkpYAvHiManeg9I',
+	access_token_key: '2261289511-ZtFpSf4N4wBw8ZTXI0RODmMkK0QWOMsbk11a0W9',
+	access_token_secret: '4JC7kn97B1KtVbff8599pIDM1Sy5myIqVceQrumnashsZ'
+}
+
---
+++ index.js
@@ -0,0 +1,52 @@
+// index.js 
+// This is the main file for Twitter Bot entry
+// This contain all the code required for twitter bot
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+var T = new Twitter(config)
+
+//search parameters
+params = {
+	q: '#nodejs',
+	count: 10,
+	result_type: 'recent',
+	lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response){
+
+	if(!err)
+	{
+		//loop through the returned tweets
+
+		for (var i = data.statuses.length - 1; i >= 0; i--) {
+
+			data.statuses[i];
+
+			let id = { id: data.statuses[i].id_str }
+
+			//Try to favorite the selected tweets
+
+			T.post('favorites/create', id, function(err, response){
+				// if favorite fails
+				if(err)
+				{ 
+					console.log(err[0].message);
+				}
+				// if favorite is successful, log the url of the tweet
+				else
+				{ 
+					let username = response.user.screen_name;
+					let tweetId = response.id_str;
+
+					console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+				}
+			})
+		}
+	}else
+	{
+		console.log(err);
+	}
+})
