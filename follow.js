245070dfdd8c6a9a1b6df41cdeef289cbe1c55c6
Ajay Singh Parmar <ajays.parmar04@gmail.com>
2019-01-06 18:08:41 +0530

    Added twitter following bot

--- follow.js
+++ follow.js
@@ -9,7 +9,7 @@ var T = new Twitter(config)
 
 //search parameters
 params = {
-	q: '#node.js',
+	q: '#IndianFootball',
 	count: 10,
 	result_type: 'recent',
 	lang: 'en'
@@ -21,30 +21,21 @@ T.get('search/tweets', params, function(err, data, response){
 	if(!err)
 	{
 		//loop through the returned tweets
-
 		for (var i = data.statuses.length - 1; i >= 0; i--) {
-
-			data.statuses[i];
-
-			let id = { id: data.statuses[i].id_str }
-
+			// record screen name here
+			screen_name =  data.statuses[i].user.screen_name;
+			
 			//Try to favorite the selected tweets
-
-			T.post('favorites/create', id, function(err, response){
-				// if favorite fails
-				if(err)
-				{ 
+			T.post('friendships/create', {screen_name}, function(err, response){
+				// if unable to follow then print error message.
+				if(err){ 
 					console.log(err[0].message);
 				}
-				// if favorite is successful, log the url of the tweet
-				else
-				{ 
-					let username = response.user.screen_name;
-					let tweetId = response.id_str;
-
-					console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+				// if favorite is successful, log the url of the tweet.
+				else{ 
+					console.log(screen_name + ': **FOLLOWED')
 				}
-			})
+			});
 		}
 	}else
 	{
